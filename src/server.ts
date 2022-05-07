39c199be812b592afc5fafdb1200723467c82841
Thiago Faccipieri <thiago.faccipieri@gmail.com>
2022-05-07 11:52:56 -0300

    JEST tests with 100% coverage on send feedback

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,15 @@
 import { routes } from './routes';
 import express from 'express';
+import cors from 'cors';
 
 const app = express();
 
-app.use(express.json())
+app.use(cors());
 
-app.use(routes)
+app.use(express.json());
+
+app.use(routes);
 
 app.listen(3333, () => {
-  console.log('ta rodando 123')
+  console.log('ta rodando');
 });
-
--- src/use-cases/submit-feedback-use-case.spec.ts
+++ src/use-cases/submit-feedback-use-case.spec.ts
@@ -1,3 +1,48 @@
-test('')
+import { SubmitFeedbackUseCase } from './submit-feedback-use-case';
 
-//1:37
+const createFeedbackSpy = jest.fn();
+const sendMailSpy = jest.fn()
+
+const submitFeedback = new SubmitFeedbackUseCase(
+  { create: createFeedbackSpy },
+  { sendMail: sendMailSpy }
+)
+
+describe('SubmitFeedback', () => {
+
+  it('should be able to submit a feedback', async () => {
+    await expect(submitFeedback.execute({
+      type: 'BUG',
+      comment: 'Example comment',
+      screenshot: 'data:image/png;base64,hfguidshgfuiysdhf',
+    })).resolves.not.toThrow();
+
+    expect(createFeedbackSpy).toHaveBeenCalled();
+    expect(sendMailSpy).toHaveBeenCalled();
+
+  });
+
+  it('should not be able to submit a feedback without a type', async () => {
+    await expect(submitFeedback.execute({
+      type: '',
+      comment: 'Example comment',
+      screenshot: 'data:image/png;base64,hfguidshgfuiysdhf',
+    })).rejects.toThrow();
+  });
+
+  it('should not be able to submit a feedback without a comment', async () => {
+    await expect(submitFeedback.execute({
+      type: 'BUG',
+      comment: '',
+      screenshot: 'data:image/png;base64,hfguidshgfuiysdhf',
+    })).rejects.toThrow();
+  });
+
+  it('should not be able to submit a feedback with a invalid screenshot', async () => {
+    await expect(submitFeedback.execute({
+      type: 'BUG',
+      comment: 'Example comment',
+      screenshot: 'image.jpg',
+    })).rejects.toThrow();
+  });
+});
